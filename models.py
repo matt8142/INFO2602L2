5d46bf2aa5937dc7399e49b60d8288e9b4c81a56
matt8142 <157655070+matt8142@users.noreply.github.com>
2025-02-07 17:52:33 +0000

    commit until slide 9

--- models.py
+++ models.py
@@ -7,11 +7,32 @@ from app import app
 db = SQLAlchemy(app)
 
 
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #set userid as a foreign key to user.id 
+  text = db.Column(db.String(255), nullable=False)
+  done = db.Column(db.Boolean, default=False)
+
+  def toggle(self):
+    self.done = not self.done
+    db.session.add(self)
+    db.session.commit()
+
+  def __init__(self, text):
+      self.text = text
+
+  def __repr__(self):
+
+    return f'<Todo: {self.id} | {self.user.username} | {self.text} | { "done" if self.done else "not done" }>'
+
+
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   password = db.Column(db.String(120), nullable=False)
+  #creates a relationship field to get the user's todos
+  todos = db.relationship('Todo', backref='user', lazy=True, cascade="all, delete-orphan")
 
   def __init__(self, username, email, password):
     self.username = username
@@ -20,7 +41,9 @@ class User(db.Model):
 
   def set_password(self, password):
     """Create hashed password."""
-    self.password = generate_password_hash(password, method='scrypt')
+    self.password = generate_password_hash(password, method='sha256')
 
   def __repr__(self):
     return f'<User {self.id} {self.username} - {self.email}>'
+
+
